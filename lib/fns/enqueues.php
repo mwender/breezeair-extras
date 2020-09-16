509da65cf09cee29899e4d4b71b9d826819d0205
Michael Wender <michael@michaelwender.com>
2020-09-16 15:16:24 -0400

    Modifying serviceareas.js to work with ACF options

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -9,11 +9,32 @@ function enqueue_scripts(){
 
   // Google Maps API
   wp_register_script( 'googlemaps', 'https://maps.googleapis.com/maps/api/js?key=' . GOOGLE_MAPS_API_KEY, null, '1.0', true );
-  $serviceAreasTimeStamp = filemtime( plugin_dir_path(__FILE__) . '../js/serviceareas.js' );
-  wp_register_script( 'serviceareas', plugin_dir_url(__FILE__) . '../js/serviceareas.js', ['googlemaps'], $serviceAreasTimeStamp, true );
+
+  // We can only overlay KMZ files that are hosted on a publically available server:
+  $hard_coded_kml_host = 'https://breezeair.net/wp-content/plugins/breezeair-extras/';
+
+  $fileTimeStamp = filemtime( plugin_dir_path(__FILE__) . '../js/serviceareas.js' );
+  wp_register_script( 'serviceareas', plugin_dir_url(__FILE__) . '../js/serviceareas.js', ['googlemaps'], $fileTimeStamp, true );
+  if( function_exists( 'have_rows') && have_rows( 'service_areas', 'option' ) ){
+    $service_areas = [];
+    while( have_rows( 'service_areas', 'option' ) ): the_row();
+      $area_name = get_sub_field( 'area_name' );
+      $latitude = get_sub_field( 'latitude' );
+      $longitude = get_sub_field( 'longitude' );
+      $zoom = get_sub_field( 'zoom' );
+      if( empty( $zoom ) )
+        $zoom = 15;
+      $service_areas[ $area_name ] = [
+        'lat'   => floatval($latitude),
+        'lng'   => floatval($longitude),
+        'zoom'  => intval( $zoom ),
+      ];
+    endwhile;
+  }
   wp_localize_script( 'serviceareas', 'wpvars', [
-      'timeStamp' => $serviceAreasTimeStamp,
-      'pluginUrl' => BREEZE_DIR_URL,
+      'timeStamp' => $fileTimeStamp,
+      'hardCodedUrl' => $hard_coded_kml_host,
+      'serviceAreas'  => $service_areas,
     ]
   );
 }
--- lib/fns/shortcodes.php
+++ lib/fns/shortcodes.php
@@ -4,10 +4,10 @@ namespace breezeair\shortcodes;
 
 function service_areas_map( $atts ){
   $args = shortcode_atts( [
-    'foo' => 'bar'
+    'map' => 'wireless-coverage',
   ], $atts );
 
-  wp_enqueue_script('serviceareas');
-  return '<div id="map"></div>';
+  wp_enqueue_script('serviceareas' );
+  return '<div class="map" id="' . $args['map'] . '"></div>';
 }
 add_shortcode( 'service_areas_map', __NAMESPACE__ . '\\service_areas_map' );
