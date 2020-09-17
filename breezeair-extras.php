e63005ae97bdfad0786d34fb416c6c6c639462c3
Michael Wender <michael@michaelwender.com>
2020-09-17 08:07:21 -0400

    Adding uber_log()

--- breezeair-extras.php
+++ breezeair-extras.php
@@ -18,3 +18,22 @@ define( 'BREEZE_DIR_URL', plugin_dir_url( __FILE__ ) );
 require_once( 'lib/fns/acf.php' );
 require_once( 'lib/fns/enqueues.php' );
 require_once( 'lib/fns/shortcodes.php' );
+
+if( ! function_exists( 'uber_log' ) ){
+  /**
+   * Enhanced logging.
+   *
+   * @param      string  $message  The log message
+   */
+  function uber_log( $message = null ){
+    static $counter = 1;
+
+    $bt = debug_backtrace();
+    $caller = array_shift( $bt );
+
+    if( 1 == $counter )
+      error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+    error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+    $counter++;
+  }
+}
